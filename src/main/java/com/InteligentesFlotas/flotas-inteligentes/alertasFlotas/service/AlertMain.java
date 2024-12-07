77c571667a83b9be430e2adbc1f4027092ac70b9
eli_pikachu <elianaperez871@gmail.com>
2024-12-07 10:33:49 -0500

    se agrego el principio de la o

--- src/main/java/com/InteligentesFlotas/flotas-inteligentes/alertasFlotas/service/AlertMain.java
+++ src/main/java/com/InteligentesFlotas/flotas-inteligentes/alertasFlotas/service/AlertMain.java
@@ -5,11 +5,11 @@ public class AlertMain {
         // Enviar alerta por correo
         Message emailAlert = new EmailAlert();
         ServiceAlert emailService = new ServiceAlert(emailAlert);
-        emailService.sendAlert("Revise", 101);
+        emailService.sendAlert("La flota nacesita reparacíones", 189282);
 
         // Enviar alerta por SMS
         Message smsAlert = new SMSAlert();
         ServiceAlert smsService = new ServiceAlert(smsAlert);
-        smsService.sendAlert("Revise el estado de su vehículo", 102);
+        smsService.sendAlert("El estado de la flota es neutro", 1829);
     }
 }
--- src/main/java/com/InteligentesFlotas/flotas-inteligentes/alertasFlotas/service/EmailAlert.java
+++ src/main/java/com/InteligentesFlotas/flotas-inteligentes/alertasFlotas/service/EmailAlert.java
@@ -2,7 +2,7 @@ package main.java.com.InteligentesFlotas.alertasFlotas.service;
 
 public class EmailAlert implements Message {
     @Override
-    public void send(String message, Integer conductorId) {
-        System.out.println("Enviando alerta por correo al conductor " + conductorId + ": " + message);
+    public void send(String message, Integer conductor) {
+        System.out.println("Enviando alerta por correo al conductor " + conductor + ": " + message);
     }
 }
