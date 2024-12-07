57c315ed3cf9e7106e9623ba469e94a79fccf331
eli_pikachu <elianaperez871@gmail.com>
2024-12-07 09:00:55 -0500

    se inicio estructura basica de carpetas, y se hizo el principio s en gestionConductor/servicios

---
+++ src/main/java/com/InteligentesFlotas/flotas-inteligentes/gestionConductores/service/conductor.java
@@ -0,0 +1,15 @@
+package main.java.com.InteligentesFlotas.flotas-inteligentes.gestionConductores.service;
+
+public class conductor {
+    // Aqui estamos aplicando el principio S principio de responsabilidad unica del solid
+    class Name{
+        String nombre;
+        String apellido;
+
+    }
+    class Addnames{
+        void nombredb(){}
+    }
+    //Podemos observar que tenemos dos clases una donde se obtiene el nombre del conductor
+    //y en la otra clase se añade a la base de datos  
+}
